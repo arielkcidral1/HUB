5b828c6dd8875a3bffd094ed329abe59521d066b
Ariel Cidral <arielkcidral1@gmail.com>
2026-07-31 20:17:08 -0300

    asd

--- supabase/migrations/20260630000400_add_contractor_document_source.sql
+++
@@ -1,9 +0,0 @@
-alter table public.hub_documentos_contratados
-add column if not exists origem_html text not null default '';
-
-alter table public.hub_documentos_contratados
-drop constraint if exists hub_documentos_contratados_origem_html_check;
-
-alter table public.hub_documentos_contratados
-add constraint hub_documentos_contratados_origem_html_check
-check (origem_html = '' or origem_html ~ '^documentos-(fredy|besten|achei|trinca)\.html$');
