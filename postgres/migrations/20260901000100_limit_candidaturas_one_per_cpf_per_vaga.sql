7007c4a3de660e934fedcb579ec0c931ca912f46
arielkcidral1 <276762144+arielkcidral1@users.noreply.github.com>
2026-09-01 19:14:09 -0300

    Remove duplicatas antes de criar indice unico em hub_candidaturas

--- postgres/migrations/20260901000100_limit_candidaturas_one_per_cpf_per_vaga.sql
+++ postgres/migrations/20260901000100_limit_candidaturas_one_per_cpf_per_vaga.sql
@@ -1,2 +1,8 @@
+delete from public.hub_candidaturas a
+using public.hub_candidaturas b
+where a.vaga_id = b.vaga_id
+  and a.cpf = b.cpf
+  and a.created_at < b.created_at;
+
 create unique index if not exists hub_candidaturas_vaga_cpf_unique
 on public.hub_candidaturas (vaga_id, cpf);
